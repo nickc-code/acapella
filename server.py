bb498407ffde6ed6824ea46668875e82b072a743
nickc-code <57518573+nickc-code@users.noreply.github.com>
2019-11-08 07:13:07 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,17 @@
-flask
+from flask import Flask
+from flask import render_template
+from flask import Response, request, jsonify
+app = Flask(__name__)
+
+
+@app.route('/')
+def Home():
+	return render_template('Home.html')
+
+
+
+
+
+
+if __name__ == '__main__':
+	app.run()
