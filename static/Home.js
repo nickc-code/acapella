8f3c0d9dcd0520923759153f5c73f8fe5ae2d55b
nickc-code <57518573+nickc-code@users.noreply.github.com>
2019-11-08 07:16:04 -0500

    Create Home.js

---
+++ static/Home.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    //when the page loads, display all the names
+    $("#exec").click(function(){  
+        window.location.href="/Exec"
+    })
+
+    $("#general").click(function(){  
+        window.location.href='/General'
+    })
+
+            // display(database)     
+})
