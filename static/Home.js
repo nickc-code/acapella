586299c2ef3cbc2c825de57d7e032314ba42c575
nickc-code <57518573+nickc-code@users.noreply.github.com>
2019-11-15 09:05:21 -0500

    Update Home.js

--- static/Home.js
+++ static/Home.js
@@ -1,11 +1,15 @@
 $(document).ready(function(){
     //when the page loads, display all the names
-    $("#exec").click(function(){  
-        window.location.href="/Exec"
-    })
+    // $("#exec").click(function(){  
+    //     window.location.href="/Exec"
+    // })
 
-    $("#general").click(function(){  
-        window.location.href='/General'
+    // $("#general").click(function(){  
+    //     window.location.href='/General'
+    // })
+
+    $("#login").click(function(){  
+        window.location.href='/UserHome'
     })
 
             // display(database)     
